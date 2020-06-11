aed637a8d396f477f8c0aef525f26ae472fcd1c6
chenkai_dell_pc <940799064@qq.com>
2020-06-11 23:05:05 +0800

    Spring Boot application.properties

--- Springboot/src/main/java/com/company/controller/HelloController.java
+++ Springboot/src/main/java/com/company/controller/HelloController.java
@@ -1,16 +1,23 @@
 package com.company.controller;
 
-
-import org.springframework.stereotype.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class HelloController {
-    @ResponseBody
+    static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+
+    @Value("${person.name}")
+    private String name;
+
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello World";
+        logger.info("hello world");
+        return String.format("Hello World, %s", name);
     }
 }
---
+++ Springboot/src/main/java/com/company/controller/TestController.java
@@ -0,0 +1,8 @@
+package com.company.controller;
+
+/**
+ * @author kaichenkai
+ * @date 2020-06-06 16:37
+ */
+public class TestController {
+}
