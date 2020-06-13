45404fa22f9b44223c899321b45cba479b98a59b
chenkai_dell_pc <940799064@qq.com>
2020-06-13 17:31:42 +0800

    restful initialize

---
+++ spring-boot-restfulcurd/src/test/java/com/atguigu/restfulcurd/SpringBootRestfulcurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.restfulcurd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulcurdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
